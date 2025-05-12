cdc0a3dde3835fbc9bc13323dabae8fbfef937a8
Agnes270 <carvalho.cecilia@escolar.ifrn.edu.br>
2025-05-12 10:17:41 -0300

    kaaaaaaaaaaaaaaaaaaaaa
    kakakakakakakakakaka

---
+++ My project/Assets/Play.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+    private Rigidbory rigididbory;
+
+    public float velocidade = 10;
+        
+    void Start()
+    {
+        rigididbory = GetComponent<Rigidbory>();
+        
+    }
+    
+    void Update()
+    {
+        Vector3 posicao = transform.position;
+        
+        
+        
+        //Esquerda x-
+        if (Input.GetKey(KeyCode.A))
+        {
+            posicao.x = transform.position.x - velocidade * Time.deltaTime;
+        }
+        
+        //Direita x-
+        if (Input.GetKey(KeyCode.D))
+        {
+            posicao.x = transform.position.x + velocidade * Time.deltaTime;
+        }
+        
+        //Cima z+
+        if (Input.GetKey(KeyCode.W))
+        {
+            posicao.z = transform.position.z + velocidade * Time.deltaTime;
+        }
+        
+        //Baixo Z-
+        if (Input.GetKey(KeyCode.S))
+        {
+            posicao.z = transform.position.z - velocidade * Time.deltaTime;
+        }
+        
+        transform.position = posicao;
+        
+        
+    }
+    
+}
+
+internal class Rigidbory
+{
+}
+
+internal class Rigibori
+{
+}
